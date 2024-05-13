aa864f67bb5be95bf2db95d8976edabdc1fcbfe2
Hatim Lahwaouir <hatimh351@gmail.com>
2024-05-13 21:23:13 +0100

    Setting up the structure of my linear regression class.

---
+++ LinearRegression.py
@@ -0,0 +1,20 @@
+class LinearRegression:
+    def __init__(self, x, y):
+        self.x  = x
+        self.y = y
+        self.o = None
+        self.b = None
+
+    #Letting the data be normalized to avoid overfitting
+    def normalization():
+        pass
+
+    #the learning phase
+    def learning():
+        pass
+
+    # the result 
+    def linearRegression():
+        pass
+
+
